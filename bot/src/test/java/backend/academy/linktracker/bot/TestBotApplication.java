06a055872bab0f0ef21d601bdfb498a24e93296d
SasniyKunchik <vlad.lipaev@gmail.com>
2026-02-20 03:25:16 +0300

    V1_bot_service_tests

--- bot/src/test/java/backend/academy/linktracker/bot/TestBotApplication.java
+++ bot/src/test/java/backend/academy/linktracker/bot/TestBotApplication.java
@@ -1,7 +1,9 @@
 package backend.academy.linktracker.bot;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.SpringApplication;
 
+@Disabled
 public class TestBotApplication {
 
     static void main(String[] args) {
