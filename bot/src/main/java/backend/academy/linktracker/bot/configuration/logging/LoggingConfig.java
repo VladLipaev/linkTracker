5d159400f90c3c123137f56b62291c4a21e8d183
SasniyKunchik <vlad.lipaev@gmail.com>
2026-08-06 15:21:10 +0300

    LT-3 interceptor + logging config

---
+++ bot/src/main/java/backend/academy/linktracker/bot/configuration/logging/LoggingConfig.java
@@ -0,0 +1,18 @@
+package backend.academy.linktracker.bot.configuration.logging;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public FilterRegistrationBean<HttpLoggingFilter> loggingFilter() {
+        FilterRegistrationBean<HttpLoggingFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new HttpLoggingFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+}
